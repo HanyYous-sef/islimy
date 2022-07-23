cda95498ae481c922e421e19d34a043a2666af43
dell <hanyy81667@gmail.com>
2022-07-23 19:55:57 +0200

    init empty screen

---
+++ android/app/src/main/kotlin/com/example/islimy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.islimy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/HomeLatout.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+static const String roudeName ='HomeScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Text('data');
+  }
+}
